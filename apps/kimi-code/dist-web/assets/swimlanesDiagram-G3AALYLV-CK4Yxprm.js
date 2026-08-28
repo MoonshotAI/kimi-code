8af956642525fafe36c602c5ccbed2a8a4ff200b
liruifengv <liruifeng1024@gmail.com>
2026-08-28 15:08:01 +0800

    Revert "chore: sync web dist from code-app"
    This reverts commit 9400a24a03863b3e8b780dda251540f824f02f3a.

--- apps/kimi-code/dist-web/assets/swimlanesDiagram-G3AALYLV-CK4Yxprm.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U-YDQ5LVB-.js";import{_ as a}from"./mermaid.core-D8UeR6T-.js";import"./chunk-5VM5RSS4-DzLUmY02.js";import"./chunk-XXDRQBXY-BeMc-h4J.js";import"./chunk-VR4S4FIN-BZ6zSN1A.js";import"./chunk-32BRIVSS-wwoCS_p_.js";import"./channel-BiD6A78K.js";import"./index-CAYChmT7.js";import"./_commonjsHelpers-CqkleIqs.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
